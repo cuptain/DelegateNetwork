7e2ef9f5f560fe16e322f0aa586e0cc137af3e95
Твой <vasvasal@gmail.com>
2019-02-12 16:42:48 +0500

    Создал классы, начал заполнять Person

---
+++ DelegateNetwork/PersonHandlerEventArgs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateNetwork
+{
+    public class PersonHandlerEventArgs
+    {
+        public string Source { get; set; } //Источник
+        public string  Changes { get; set; } //Что произошло
+        public DateTime Time { get; set; } //Время события
+        public string Information { get; set; } //Доп. информация
+
+        public PersonHandlerEventArgs(string _source, string _changes, DateTime _time, string _information) //Конструктор
+        {
+            Source = _source;
+            Changes = _changes;
+            Time = _time;
+            Information = _information;
+        }
+
+        public override string ToString() //Нормальный вывод
+        {
+            string output = "";
+            output += "Сообщение о событии у " + Source + "\n";
+            output += "        Тип события: " + Changes + "\n";
+            output += "        Что произошло: " + Information + "\n";
+            output += "        Время: " + Time.ToShortTimeString() + " " + Time.ToShortDateString() + "\n";
+            return output;
+        }
+    }
+}
